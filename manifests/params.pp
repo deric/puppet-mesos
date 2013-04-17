a9da5841602427c328fe192f2df4a5ced275024c
Tomas Barton <barton.tomas@gmail.com>
2013-04-18 00:17:31 +0200

    added params

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,8 +10,14 @@
 #
 # Sample Usage:
 #
-class storm::params {
-   
+class mesos::params {
+  
+  # human readable name for cluster
+  $cluster      = hiera('cluster', '')
   $mesos_master = hiera('master', '127.0.0.1')
-
+  $master_port  = hiera('master_port', 5050)
+  
+  # master and slave creates separate logs automatically
+  $log_dir      = hiera('log_dir', '/var/log/mesos')
+  $conf_dir     = hiera('conf_dir', '/etc/mesos/conf')
 }
