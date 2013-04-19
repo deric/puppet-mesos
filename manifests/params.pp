f7df23098e7a7bd59b18a63893997acda5bf2b2b
Tomas Barton <barton.tomas@gmail.com>
2013-04-19 23:23:58 +0200

    using params lookup

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,11 +13,11 @@
 class mesos::params {
   
   # human readable name for cluster
-  $cluster      = hiera('cluster', '')
-  $master = hiera('master', '127.0.0.1')
-  $master_port  = hiera('master_port', 5050)
+  $cluster      = params_lookup('cluster', '')
+  $master       = params_lookup('master', '127.0.0.1')
+  $master_port  = params_lookup('master_port', 5050)
   
   # master and slave creates separate logs automatically
-  $log_dir      = hiera('log_dir', '/var/log/mesos')
-  $conf_dir     = hiera('conf_dir', '/etc/mesos')
+  $log_dir      = params_lookup('log_dir', '/var/log/mesos')
+  $conf_dir     = params_lookup('conf_dir', '/etc/mesos')
 }
