9c8b2d888937bc19b01bd923ac5d287c3e51e68b
Tomas Barton <barton.tomas@gmail.com>
2013-04-19 23:53:54 +0200

    master port as text

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,7 +15,7 @@ class mesos::params {
   # human readable name for cluster
   $cluster      = params_lookup('cluster', '')
   $master       = params_lookup('master', '127.0.0.1')
-  $master_port  = params_lookup('master_port', 5050)
+  $master_port  = params_lookup('master_port', '5050')
   
   # master and slave creates separate logs automatically
   $log_dir      = params_lookup('log_dir', '/var/log/mesos')
