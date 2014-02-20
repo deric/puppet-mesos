837eb877b3f62becfaf61cf21f467f1a257108ab
krall <krall@yelp.com>
2014-02-19 21:27:49 -0800

    Pull out IP-picking magic, allow it to be passed in when including mesos::slave or mesos::master instead.

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,19 +18,20 @@
 # Copyright 2013-2014 Tomas Barton
 #
 class mesos(
-  $ensure       = hiera('mesos::version', 'present')
+  $ensure         = hiera('mesos::version', 'present')
 ) {
   # master and slave creates separate logs automatically
   # TODO: currently not used
-  $log_dir      = hiera('mesos::log_dir', '/var/log/mesos')
-  $conf_dir     = hiera('mesos::conf_dir', '/etc/mesos')
+  $log_dir        = hiera('mesos::log_dir', '/var/log/mesos')
+  $conf_dir       = hiera('mesos::conf_dir', '/etc/mesos')
   # e.g. zk://localhost:2181/mesos
-  $zookeeper    = hiera('mesos::zookeeper', '')
+  $zookeeper      = hiera('mesos::zookeeper', '')
   # if "zk" is empty, master value is used
-  $master       = hiera('mesos::master', '127.0.0.1')
-  $master_port  = hiera('mesos::master_port', '5050')
-  $owner        = hiera('mesos::owner', 'root')
-  $group        = hiera('mesos::group', 'root')
+  $master         = hiera('mesos::master', '127.0.0.1')
+  $master_port    = hiera('mesos::master_port', '5050')
+  $owner          = hiera('mesos::owner', 'root')
+  $group          = hiera('mesos::group', 'root')
+  $listen_address = hiera('mesos::listen_address', $::ipaddress)
 
   class {'mesos::install':
     ensure => $ensure,
