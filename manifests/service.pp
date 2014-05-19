24c1b773a39dadf1c79f9ba5893c2f09b04c64fa
Sean McLaughlin <seanmcl@egmont.local>
2014-05-19 17:48:15 -0400

    Use the 'upstart' service on centos.

--- manifests/service.pp
+++ manifests/service.pp
@@ -16,6 +16,7 @@ define mesos::service(
     hasstatus  => true,
     hasrestart => true,
     enable     => $enable,
+    provider   => 'upstart',
     subscribe  => [ File['/etc/default/mesos'],
       File["/etc/default/mesos-${name}"]
     ],
