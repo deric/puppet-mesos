55cf41540c52ec81927f24be203e7be644bd3fc0
William Leese <william.leese@meltwater.com>
2014-07-07 17:38:23 +0900

    additional unit test for rhel7

--- spec/defines/service_spec.rb
+++ spec/defines/service_spec.rb
@@ -8,6 +8,7 @@ describe 'mesos::service' do
       :osfamily => family,
       :operatingsystem => os,
       :lsbdistcodename => codename,
+      :os_maj_version => codename,
     }}
 
     it { should contain_service('mesos-slave').with(
@@ -34,10 +35,13 @@ describe 'mesos::service' do
     it_behaves_like 'mesos-service', 'Debian', 'Ubuntu', 'precise'
   end
 
+  context 'on red-hat-like system < 7' do
+    it_behaves_like 'mesos-service', 'RedHat', 'RedHat', 6, 'upstart'
+    it_behaves_like 'mesos-service', 'RedHat', 'CentOS', 6, 'upstart'
+  end
   context 'on red-hat-like system' do
-    # last argument should be service provider
-    it_behaves_like 'mesos-service', 'RedHat', 'RedHat', '6', 'upstart'
-    it_behaves_like 'mesos-service', 'RedHat', 'CentOS', '6', 'upstart'
+    it_behaves_like 'mesos-service', 'RedHat', 'RedHat', 7, nil
+    it_behaves_like 'mesos-service', 'RedHat', 'CentOS', 7, nil
   end
 
 end
