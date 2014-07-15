0d0d6d15577c77f69fddafbacbf2267bb978c03c
William Leese <william.leese@meltwater.com>
2014-07-15 16:18:57 +0900

    remove provider mangling

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,17 +11,11 @@ define mesos::service(
   $enable = false,
 ) {
 
-  if $::osfamily == 'RedHat' and $::os_maj_version < 7 { 
-    $provider = 'upstart'
-  } else {
-    $provider = undef
-  }
   service { "mesos-${name}":
     ensure     => 'running',
     hasstatus  => true,
     hasrestart => true,
     enable     => $enable,
-    provider   => $provider,
     subscribe  => [ File['/etc/default/mesos'],
       File["/etc/default/mesos-${name}"]
     ],
