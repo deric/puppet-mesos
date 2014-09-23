55f08d5f99090e4a2024d5819780a2c391d4f0c0
Tomas Barton <barton.tomas@gmail.com>
2014-09-23 20:14:29 +0200

    removed explicit hiera calls

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,27 +18,32 @@
 # Copyright 2013-2014 Tomas Barton
 #
 class mesos(
-  $ensure         = hiera('mesos::version', 'present')
-) {
+  $ensure         = 'present',
+  $version        = undef,
   # master and slave creates separate logs automatically
   # TODO: currently not used
-  $log_dir        = hiera('mesos::log_dir', '/var/log/mesos')
-  $conf_dir       = hiera('mesos::conf_dir', '/etc/mesos')
+  $log_dir        = '/var/log/mesos',
+  $conf_dir       = '/etc/mesos',
   # e.g. zk://localhost:2181/mesos
-  $zookeeper      = hiera('mesos::zookeeper', '')
+  $zookeeper      = '',
   # if "zk" is empty, master value is used
-  $master         = hiera('mesos::master', '127.0.0.1')
-  $master_port    = hiera('mesos::master_port', '5050')
-  $owner          = hiera('mesos::owner', 'root')
-  $group          = hiera('mesos::group', 'root')
-  $listen_address = hiera('mesos::listen_address', $::ipaddress)
-  $repo           = hiera('mesos::repo', undef)
-  $env_var        = hiera('mesos::env_var', {})
-
+  $master         = '127.0.0.1',
+  $master_port    = 5050,
+  $owner          = 'root',
+  $group          = 'root',
+  $listen_address = $::ipaddress,
+  $repo           = undef,
+  $env_var        = {},
+) {
   validate_hash($env_var)
 
+  $mesos_ensure = $version ? {
+    undef    => $ensure,
+    default  => $version,
+  }
+
   class {'mesos::install':
-    ensure      => $ensure,
+    ensure      => $mesos_ensure,
     repo_source => $repo,
   }
 
