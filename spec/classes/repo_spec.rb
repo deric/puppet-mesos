240033d3613ad3be09eafec3a47ef1c61a7ecaba
Tomas Barton <barton.tomas@gmail.com>
2015-07-02 13:21:03 +0200

    fix repo specs (#39)

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -19,7 +19,7 @@ describe 'mesos::repo', :type => :class do
      'location'    => "http://repos.mesosphere.io/#{operatingsystem.downcase}",
      'repos'       => 'main',
      'release'     => "#{lsbdistcodename}",
-     'key'         => '81026D0004C44CF7EF55ADF8DF7D54CBE56151BF',
+     'key'         => {'id'=>'81026D0004C44CF7EF55ADF8DF7D54CBE56151BF', 'server' => 'subkeys.pgp.net'},
      'include_src' => false
     )}
 
