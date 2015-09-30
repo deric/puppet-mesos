d2625256ec046afa2a7b3464b555b6b3b2003e19
taik0 <suikogaiden@gmail.com>
2015-09-30 17:08:33 +0200

    Remove resource ordering
    The resource ordering is already done in the package definition, here we should only add an apt-get update.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -30,7 +30,6 @@ class mesos::repo(
               include  => { 'src' => false }
             }
           include apt::update
-          Exec['apt_update'] -> Package['mesos']
           }
           default: {
             notify { "APT repository '${source}' is not supported for ${::osfamily}": }
